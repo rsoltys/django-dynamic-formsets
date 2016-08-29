aa18c6a8e07b72a9d168e8c7da86afa9731e5c2f
Allan James Vestal <allanjamesvestal@gmail.com>
2016-08-29 12:26:36 -0500

    Added necessary files for PyPI upload.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup  # NOQA
+
+
+REPO_URL = 'https://github.com/dallasmorningnews/django-dynamic-formsets'
+
+
+setup(
+    name='django-dynamic-formsets',
+    packages=['django-dynamic-formsets'],
+    version='0.0.1',
+    description='A random test lib',
+    author='Allan James Vestal, The Dallas Morning News',
+    author_email='ajvestal@dallasnews.com',
+    url=REPO_URL,
+    download_url=REPO_URL + '/tarball/0.0.1',
+    keywords=['django', 'formset', 'javascript'],
+    classifiers=[],
+)
